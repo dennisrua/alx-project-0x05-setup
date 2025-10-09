c8a69c492f5b496fa8e7f27b88369a494836abe5
Dennis Rua <ruadennis20@gmail.com>
2025-10-09 17:57:45 +0300

    feat: set up local environment with .env.local and GPT API key configuration

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleGeneratedImage = async () => {
-    console.log('Generating Image');
+    console.log('Generating Image...');
     console.log(process.env.NEXT_PUBLIC_GPT_API_KEY);
   };
 
